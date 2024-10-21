a3d1d265d11920797d2b3b5153e6e5cda3ddaec8
JoyCode4 <joywadhonkar4@gmail.com>
2024-10-21 12:25:35 +0530

    Data Binding - Property(One Way Binding- Component to view)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   empLastName:string="Wadhonkar";
   a:number=12;
   b:number=67;
+  angularImg:string="https://angular.io/assets/images/logos/angular/angular.png";
 
   constructor(){
     this.students=[{id:1,name: "Student1",gender:"M",age:24,course:"BCA"},{id:2,name: "Student2",gender:"F",age:22,course:"MBA"},{id:3,name: "Student3",gender:"M",age:26,course:"BSc"},{id:4,name: "Student4",gender:"F",age:20,course:"MCA"}];
