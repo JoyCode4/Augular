2a8b8257e6d4436995f7b88096622c470d1f5041
JoyCode4 <joywadhonkar4@gmail.com>
2024-10-21 12:15:11 +0530

    Data Binding - Interpolation(One Way Binding- Component to view)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,10 @@ export class AppComponent {
   //   alert("hello....!");
   // }
   isValid:boolean=true;
+  empFirstName:string="Jayesh";
+  empLastName:string="Wadhonkar";
+  a:number=12;
+  b:number=67;
 
   constructor(){
     this.students=[{id:1,name: "Student1",gender:"M",age:24,course:"BCA"},{id:2,name: "Student2",gender:"F",age:22,course:"MBA"},{id:3,name: "Student3",gender:"M",age:26,course:"BSc"},{id:4,name: "Student4",gender:"F",age:20,course:"MCA"}];
