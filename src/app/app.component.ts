ed853f531a1eb1df83b683265bd29db4e9d2883e
JoyCode4 <joywadhonkar4@gmail.com>
2024-10-21 10:31:09 +0530

    Usage of TrackBy with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,16 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'Angular Application';
   name="";
+  students:Student[]=[];
   // @HostListener("click",['$event'])
   // show(){
   //   alert("hello....!");
   // }
   isValid:boolean=true;
 
+  constructor(){
+    this.students=[{id:1,name: "Student1",gender:"M",age:24,course:"BCA"},{id:2,name: "Student2",gender:"F",age:22,course:"MBA"},{id:3,name: "Student3",gender:"M",age:26,course:"BSc"},{id:4,name: "Student4",gender:"F",age:20,course:"MCA"}];
+  }
   changeValue(isValid:boolean){
     this.isValid=isValid;
   }
@@ -24,9 +28,18 @@ export class AppComponent {
     console.log(this.name)
   }
 
-  students:Student[]=[{id:1,name: "Student1"},{id:2,name: "Student2"},{id:3,name: "Student3"},{id:4,name: "Student4"}];
+  getMoreData(){
+    this.students=[{id:1,name: "Student1",gender:"M",age:24,course:"BCA"},{id:2,name: "Student2",gender:"F",age:22,course:"MBA"},{id:3,name: "Student3",gender:"M",age:26,course:"BSc"},{id:4,name: "Student4",gender:"F",age:20,course:"MCA"},{id:5,name: this.name, gender:"M",age:25,course:"BCA"}];
+  }
+
+  trackById(index:number,student:Student):number{
+    return student.id;
+  }
 }
 interface Student{
   id:number,
   name:string,
+  gender:string,
+  age:number,
+  course:string
 }
