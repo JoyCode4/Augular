cd47eab774b1476bc5ba9e13c9ecfe1349599ccc
JoyCode4 <joywadhonkar4@gmail.com>
2024-10-22 15:44:26 +0530

    Services Angular - Part 3 - Singleton Object of services using provideIn in Injectable

--- src/app/numlist.service.ts
+++ src/app/numlist.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable({
   // when we provide service in root then we can use that service in any component.as we have not assign or import the component in app.module.ts
-  providedIn: 'root'})
+  providedIn: 'root'
+})
 // @Injectable()
 export class NumlistService {
   list:number[]=[100];
